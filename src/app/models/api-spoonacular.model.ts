8ade7748c23f586fc94d3e9d18f4a498d0713b45
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-13 12:01:52 +0200

    add: model for spoonacular api

---
+++ src/app/models/api-spoonacular.model.ts
@@ -0,0 +1,85 @@
+export interface SpoonacularApiData {
+  recipes: Recipe[];
+}
+
+export interface Recipe {
+  id: number;
+  title: string;
+  readyInMinutes: number;
+  servings: number;
+  image: string;
+  vegetarian: boolean;
+  vegan: boolean;
+  glutenFree: boolean;
+  dairyFree: boolean;
+  cheap: boolean;
+  sustainable: boolean;
+  sourceName: string;
+  extendedIngredients: ExtendedIngredient[];
+  summary: string;
+  cuisines?: string[];
+  dishTypes: string[];
+  diets?: string[];
+  instructions: string;
+  analyzedInstructions: AnalyzedInstruction[];
+}
+
+export interface ExtendedIngredient {
+  id: number;
+  aisle?: string;
+  image: string;
+  name: string;
+  nameClean: string;
+  originalString: string;
+  amount: number;
+  unit: string;
+  measures: Measures;
+}
+
+export interface Measures {
+  us: Us;
+  metric: Metric;
+}
+
+export interface Us {
+  amount: number;
+  unitShort: string;
+  unitLong: string;
+}
+
+export interface Metric {
+  amount: number;
+  unitShort: string;
+  unitLong: string;
+}
+
+export interface AnalyzedInstruction {
+  steps: Step[];
+}
+
+export interface Step {
+  number: number;
+  step: string;
+  ingredients: Ingredient[];
+  equipment: Equipment[];
+  length: Length;
+}
+
+export interface Ingredient {
+  id: number;
+  name: string;
+  localizedName: string;
+  // image: string;
+}
+
+export interface Equipment {
+  id: number;
+  name: string;
+  localizedName: string;
+  // image: string;
+}
+
+export interface Length {
+  number: number;
+  unit: string;
+}
