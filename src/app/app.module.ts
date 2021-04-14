29546311c8efad840f36aad79c361c44f2c51860
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-14 12:23:43 +0200

    test w interceptor for connecting to multiple APIs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 
+import { ApiInterceptor } from '../app/interceptors/api-interceptor.interceptor';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecipesComponent,
-    RecipeComponent
+  declarations: [AppComponent, RecipesComponent, RecipeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
