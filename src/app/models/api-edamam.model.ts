96c662992cb36736660e90d20d1568b404bc8250
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-15 12:40:30 +0200

    comment out old model

--- src/app/models/api-edamam.model.ts
+++ src/app/models/api-edamam.model.ts
@@ -1,19 +1,19 @@
-export interface EdamamApiData {
-  hits: Hit[];
-}
+// export interface EdamamApiData {
+//   hits: Hit[];
+// }
 
-export interface Hit {
-  recipe: Recipe;
-}
+// export interface Hit {
+//   recipe: Recipe;
+// }
 
-export interface Recipe {
-  label: string; // recipe title
-  image: string; // image url
-  yield: number; // number of servings
-  ingredientLines: string[]; // each string = type + amount
-  healthLabels?: string[]; // eg milk-free, gluten-free
-  cautions?: string[]; // eg milk, soy, nuts
-  cuisineType?: string; // eg chinese, indian
-  mealType?: string; // eg  lunch, dinner, breakfast, snack
-  dishType?: string; // eg dessert, soup, salad, sandwich
-}
+// export interface Recipe {
+//   label: string; // recipe title
+//   image: string; // image url
+//   yield: number; // number of servings
+//   ingredientLines: string[]; // each string = type + amount
+//   healthLabels?: string[]; // eg milk-free, gluten-free
+//   cautions?: string[]; // eg milk, soy, nuts
+//   cuisineType?: string; // eg chinese, indian
+//   mealType?: string; // eg  lunch, dinner, breakfast, snack
+//   dishType?: string; // eg dessert, soup, salad, sandwich
+// }
