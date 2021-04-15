2c44345dc6ae5615d62f72091217ccf6864be153
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-15 22:20:27 +0200

    add: further details to recipe page

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 
-import { Recipe } from 'src/app/models/api-spoonacular.model';
+import { Recipe, ExtendedIngredient } from 'src/app/models/api-spoonacular.model';
 import { RecipesService } from '../../services/recipes.service';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.scss']
+  styleUrls: ['./recipe.component.scss'],
 })
 export class RecipeComponent implements OnInit {
   recipe: Recipe;
   chosenRecipe;
   id: number | string;
+  extendedIngredients: ExtendedIngredient[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -26,6 +27,10 @@ export class RecipeComponent implements OnInit {
       .getDetailedRecipe(this.id)
       .subscribe((Recipe) => {
         this.recipe = Recipe;
+        this.extendedIngredients = Recipe.extendedIngredients;
+        console.log(this.recipe);
+        console.log(this.extendedIngredients);
       });
+    
   }
 }
