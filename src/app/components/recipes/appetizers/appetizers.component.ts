6186d4cc1258497797e6692aa59b329f58facb21
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-18 14:00:22 +0200

    add: separate components for dish type views

---
+++ src/app/components/recipes/appetizers/appetizers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appetizers',
+  templateUrl: './appetizers.component.html',
+  styleUrls: ['./appetizers.component.scss']
+})
+export class AppetizersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
