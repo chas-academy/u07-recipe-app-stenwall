dc284ceed1631ca85b2e65f7731ae4ab3e974bd5
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-21 10:23:03 +0200

    fix: tab navigation recipe-page (working on init)

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
-import { Recipe, ExtendedIngredient } from 'src/app/models/api-spoonacular.model';
+import {
+  Recipe,
+  ExtendedIngredient,
+} from 'src/app/models/api-spoonacular.model';
 import { RecipesService } from '../../services/recipes.service';
 
 @Component({
@@ -15,9 +17,8 @@ export class RecipeComponent implements OnInit {
   chosenRecipe;
   id: number | string;
   extendedIngredients: ExtendedIngredient[] = [];
-
-  links = ['Ingredients', 'Instructions'];
-  activeLink = this.links[0];
+  data;
+  selectedTab = 'ingredients';
 
   constructor(
     private route: ActivatedRoute,
@@ -26,13 +27,37 @@ export class RecipeComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+    this.route.data.subscribe((data) => (this.data = data));
+
     this.chosenRecipe = this.recipesService
       .getDetailedRecipe(this.id)
       .subscribe((Recipe) => {
         this.recipe = Recipe;
         this.extendedIngredients = Recipe.extendedIngredients;
-        console.log(this.recipe);
-        console.log(this.extendedIngredients);
       });
+
+    this.onTabChange();
+  }
+
+  onChange(event) {
+    this.selectedTab = event.tab.textLabel;
+    this.onTabChange();
+    console.log(event.tab.textLabel);
+  }
+
+  onTabChange() {
+    if (this.selectedTab == 'ingredients') {
+      console.log(this.selectedTab);
+    } else if (this.selectedTab == 'instructions') {
+      console.log(this.selectedTab);
+    }
   }
 }
+
+// sources to tab selection solution:
+// -----------------------------------------------------------------------
+// https://stackoverflow.com/questions/61761665/angular-mattabchangeevent-does-not-trigger-on-page-load
+// -----------------------------------------------------------------------
+// https://stackoverflow.com/questions/50854238/angular-material-5-how-to-call-a-function-when-a-tab-is-selected-clicked
+// -----------------------------------------------------------------------
+// https://stackblitz.com/edit/angular-q2pgt1?file=app%2Ftabs-overview-example.ts
