cb040c8dae68ff1e03b7d6733f2abc059939d716
Karin Stenwall <karin.stenwall@gmail.com>
2021-04-28 18:56:02 +0200

    solve tab change small screen

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -10,11 +10,13 @@ import { RecipesService } from '../../services/recipes.service';
 import {
   MatBottomSheet,
   MatBottomSheetConfig,
+  MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
 import { Overlay } from '@angular/cdk/overlay';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 import { BottomSheetComponent } from '../bottom-sheet/bottom-sheet.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipe',
@@ -27,14 +29,15 @@ export class RecipeComponent implements OnInit {
   id: number | string;
   extendedIngredients: ExtendedIngredient[] = [];
   data;
-  selectedTab = 'ingredients';
+  selectedTab: string;
   configBottomSheet: MatBottomSheetConfig = {
     closeOnNavigation: true,
     hasBackdrop: true,
-    // backdropClass: ,
     direction: 'ltr',
   };
   isLargeScreen: boolean;
+  isIngredientsOpen = false;
+  isInstructionsOpen = false;
 
   @ViewChild('templateBottomSheet') TemplateBottomSheet: TemplateRef<any>;
 
@@ -42,14 +45,26 @@ export class RecipeComponent implements OnInit {
     private route: ActivatedRoute,
     private recipesService: RecipesService,
     private bottomSheet: MatBottomSheet,
+    private bottomSheetRef: MatBottomSheetRef<TemplateRef<any>>,
     private overlay: Overlay,
-    private breakpointObserver: BreakpointObserver
-  ) {}
+    public breakpointObserver: BreakpointObserver
+  ) {
+    // check the size of the screen
+    breakpointObserver.observe(['(max-width: 719px)']).subscribe((result) => {
+      if (result.matches) {
+        this.isLargeScreen = false;
+      } else {
+        this.isLargeScreen = true;
+      }
+    });
+  }
 
   ngOnInit(): void {
+    // save id from get-params
     this.id = this.route.snapshot.paramMap.get('id');
     this.route.data.subscribe((data) => (this.data = data));
 
+    // save recipe details based on id
     this.chosenRecipe = this.recipesService
       .getDetailedRecipe(this.id)
       .subscribe((Recipe) => {
@@ -57,23 +72,48 @@ export class RecipeComponent implements OnInit {
         this.extendedIngredients = Recipe.extendedIngredients;
       });
 
-    this.onTabChange();
+    // run on init to make "ingredient" the deafult tab
+    // this.onTabChange();
 
-    this.isLargeScreen = this.breakpointObserver.isMatched('(min-width: 992px)');
+    // this.bottomSheetRef
+    //   .afterOpened()
+    //   .pipe(() => (this.bottomSheetRef = undefined));
+
+    // this.bottomSheetRef.afterOpened().subscribe((result) => {
+    //   this.isTabOpen = true;
+    //   console.log('widgetEditorModal has been opened!');
+    // });
   }
 
-  onChange(event) {
+  // onTabChange(event) {
+  //   this.selectedTab = event.tab.textLabel;
+  //   this.onTabChange();
+  // }
+
+  setTab(event) {
+    console.log(event);
+  }
+
+  onTabChange(event) {
     this.selectedTab = event.tab.textLabel;
-    this.onTabChange();
-    console.log(event.tab.textLabel);
+    if (this.selectedTab == 'ingredients') {
+      this.isIngredientsOpen = true;
+    } else if (this.selectedTab == 'instructions') {
+      this.isInstructionsOpen = false;
+    }
   }
 
-  onTabChange() {
+
+  onChange() {
     if (this.selectedTab == 'ingredients') {
-      console.log(this.selectedTab);
+      this.isIngredientsOpen = true;
     } else if (this.selectedTab == 'instructions') {
-      console.log(this.selectedTab);
+      this.isInstructionsOpen = true;
     }
+  }
+
+  openBottomSheet() {
+    const scrollStrategy = this.overlay.scrollStrategies.block();
   }
 
   // openBottomSheet() {
@@ -81,12 +121,74 @@ export class RecipeComponent implements OnInit {
   //   this.bottomSheet.open(BottomSheetComponent, scrollStrategy);
   // }
 
-  openTemplateSheetMenu() {
-    const scrollStrategy = this.overlay.scrollStrategies.reposition();
-    this.bottomSheet.open(this.TemplateBottomSheet, { scrollStrategy });
+  openBottomSheetTest(event) {
+    console.log(event.target.innerText);
+    this.selectedTab = event.target.innerText.toLowerCase();
+    console.log(this.selectedTab);
+    const scrollStrategy = this.overlay.scrollStrategies.block();
+    console.log(
+      `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+    );
+    // this.bottomSheetRef.afterOpened().subscribe((result) => {
+    //   console.log(result);
+    //   console.log('Bottom sheet has been opened.');
+    // });
+    // if (!this.isBottomSheetOpen) {
+    //   this.bottomSheet.open(this.TemplateBottomSheet, { scrollStrategy });
+    //   this.isBottomSheetOpen = true;
+    // } else if (this.isBottomSheetOpen) {
+    //   this.bottomSheet.dismiss();
+    //   this.isBottomSheetOpen = false;
+    // } else {
+    //   this.bottomSheet.open(this.TemplateBottomSheet, { scrollStrategy });
+    //   this.isBottomSheetOpen = true;
+    // }
+    if (!this.isIngredientsOpen && this.selectedTab === 'ingredients') {
+      this.bottomSheet.open(this.TemplateBottomSheet, { scrollStrategy });
+      this.isIngredientsOpen = true;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    } else if (this.isIngredientsOpen && this.selectedTab === 'ingredients') {
+      this.bottomSheet.dismiss();
+      this.isIngredientsOpen = false;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    } else if (this.isIngredientsOpen && this.selectedTab === 'instructions') {
+      this.isInstructionsOpen = true;
+      this.isIngredientsOpen = false;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    } else if (this.isInstructionsOpen && this.selectedTab === 'ingredients') {
+      this.isInstructionsOpen = false;
+      this.isIngredientsOpen = true;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    } else if (
+      !this.isInstructionsOpen &&
+      this.selectedTab === 'instructions'
+    ) {
+      this.bottomSheet.open(this.TemplateBottomSheet, { scrollStrategy });
+      this.isInstructionsOpen = true;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    } else if (this.isInstructionsOpen && this.selectedTab === 'instructions') {
+      this.bottomSheet.dismiss();
+      this.isInstructionsOpen = false;
+      console.log(
+        `Ingredients-tab is ${this.isIngredientsOpen}, instructions-tab is ${this.isInstructionsOpen} and selected tab is ${this.selectedTab}`
+      );
+    }
+    // } else (!this.isInstructionsOpen && !this.isIngredientsOpen && this.selectedTab === undefined) {
+
+    // }
   }
 
-  closeTemplateSheetMenu() {
+  closeBottomSheet() {
     this.bottomSheet.dismiss();
   }
 }
@@ -102,3 +204,8 @@ export class RecipeComponent implements OnInit {
 // sources to bottom sheet on mobile:
 // -----------------------------------------------------------------------
 // https://stackoverflow.com/questions/49651320/how-to-use-scrollstrategy-in-matdialog
+
+// sources to breakpoint-observer/solution:
+// -----------------------------------------------------------------------
+// https://material.angular.io/cdk/layout/api#Breakpoints
+// https://www.techiediaries.com/angular/responsive-image-breakpoints-cdk-breakpointobserver-angular-9-8/
