aa22b9d30b6bb924ab2df57610061aa5ce577871
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-20 23:25:41 +0200

    env-test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-
-  SPOON_API_URL: 'undefined',
-  SPOON_API_KEY: 'undefined',
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
 
-  SPOON_API_URL: 'undefined',
-  SPOON_API_KEY: 'undefined',
 };
