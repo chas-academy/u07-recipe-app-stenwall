606a73be9f715c900373c6f46ad07da6ef43f79b
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-20 23:10:01 +0200

    comment out interceptor component

--- src/app/interceptors/api-interceptor.interceptor.ts
+++ src/app/interceptors/api-interceptor.interceptor.ts
@@ -1,38 +1,38 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+// import { Injectable } from '@angular/core';
+// import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { environment } from '../../environments/environment';
 
-@Injectable()
+// @Injectable()
 
-// THIS COMPONENT IS NOT USED!
-// I just kept it for reference
+// // THIS COMPONENT IS NOT USED!
+// // I just kept it for reference
 
-export class ApiInterceptor implements HttpInterceptor {
+// export class ApiInterceptor implements HttpInterceptor {
 
-  constructor() {}
+//   constructor() {}
 
-  intercept(
-    request: HttpRequest<unknown>, 
-    next: HttpHandler
-    ): Observable<HttpEvent<unknown>> {
-      // get the request url
-      let requestUrl = request.url;
-      // replace the string prefix with the correct url
-      if (requestUrl.indexOf('@api-eda') !== -1) {
-        requestUrl = requestUrl.replace('@api-eda', environment.EDA_API_URL);
-      }
-       // clone the http request
-      const correctRequest = request.clone({
-        url: requestUrl
-      });
-      // return clone
-      return next.handle(correctRequest);
-  }
-}
+//   intercept(
+//     request: HttpRequest<unknown>, 
+//     next: HttpHandler
+//     ): Observable<HttpEvent<unknown>> {
+//       // get the request url
+//       let requestUrl = request.url;
+//       // replace the string prefix with the correct url
+//       if (requestUrl.indexOf('@api-eda') !== -1) {
+//         requestUrl = requestUrl.replace('@api-eda', environment.EDA_API_URL);
+//       }
+//        // clone the http request
+//       const correctRequest = request.clone({
+//         url: requestUrl
+//       });
+//       // return clone
+//       return next.handle(correctRequest);
+//   }
+// }
 
-// sources for this solution when using multiple API:s:
-// -----------------------------------------------------------------------
-// https://dev.to/leonardovff/angular-consuming-multiple-apis-with-angular-httpclient-in-a-beautiful-way-using-interceptors-3im1
-// -----------------------------------------------------------------------
-// https://angular.io/guide/http#intercepting-requests-and-responses
+// // sources for this solution when using multiple API:s:
+// // -----------------------------------------------------------------------
+// // https://dev.to/leonardovff/angular-consuming-multiple-apis-with-angular-httpclient-in-a-beautiful-way-using-interceptors-3im1
+// // -----------------------------------------------------------------------
+// // https://angular.io/guide/http#intercepting-requests-and-responses
