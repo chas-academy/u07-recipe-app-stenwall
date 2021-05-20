c3c04c8805def034425ee02aae95e35ef279621c
Karin Stenwall <karin.stenwall@gmail.com>
2021-05-20 21:36:15 +0200

    cleanup code: recipe-card

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
 import { Recipe } from 'src/app/models/api-spoonacular.model';
 import { ListService } from '../../services/list.service';
 
@@ -18,24 +24,23 @@ export class RecipeCardComponent implements OnInit, OnChanges {
     this.showRecipes = this.data;
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['data']) {
       this.showRecipes = this.data;
     }
   }
 
-  addRecipeToList(event, id, title, image) {
+  addRecipeToList(event: any, id: number, title: string, image: string): void {
     event.stopPropagation();
     this.listService.addToList(id, title, image);
   }
 
-  removeRecipeFromList(event, id) {
+  removeRecipeFromList(event: any, id: number): void {
     event.stopPropagation();
     this.listService.removeFromList(id);
   }
 
   isRecipeSaved(id: number): boolean {
-    console.log(this.listService.checkIfRecipeInList(id));
     return this.listService.checkIfRecipeInList(id);
   }
 }
