0003350f297fccb36ba5ce829a0a00fa34fe1c49
Karin Stenwall <karin.stenwall@gmail.com>
2021-08-17 22:40:40 +0200

    update: list sevice with crud for everything

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { List, ListData } from '../models/list.model';
+import { ListRecipe, ListRecipeData } from '../models/list-recipe.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,55 +12,95 @@ export class ListService {
   private u08ApiUrl: string;
   lists: List[];
   list: List;
-  isRecipeInList: boolean;
-  // recipeInListChange: Subject<boolean> = new Subject<boolean>();
+  // isRecipeInList;
+  // recipeInListChange;
+  // recipeInListChange: Subject<any> = new Subject<any>();
 
   constructor(
     private http: HttpClient
   ) {
     this.u08ApiUrl = environment.U08_API_URL;
-    // this.recipeInListChange.subscribe((value) => (this.isRecipeInList = value));
+    // this.recipeInListChange.subscribe((data) => (this.checkIfRecipeInList(5, 5) = data.exists));
+    // this.recipeInListChange = this.checkIfRecipeInList(5, 2).subscribe((data) => {
+    //   this.isRecipeInList = data;
+    //   console.log(this.isRecipeInList);
+    // })
   }
 
+  // get all lists belonging to logged in user
   getAllLists(): Observable<List[]> {
     return this.http.get<ListData['list']>(`${this.u08ApiUrl}/api/lists`);
   }
 
-  getList(id: number | string): Observable<List> {
-    return this.http.get<List>(`${this.u08ApiUrl}/api/lists/${id}`);
-  }
-
+  // create a new list
   addNewList(title: string): Observable<any> {
     return this.http.post(`${this.u08ApiUrl}/api/lists`, title);
   }
 
-  // addNewList(title: string, user_id: number | string): Observable<any> {
-  //   return this.http.post('http://u08-recipe-api.test/api/lists/store', title);
+  // update given list
+  updateList(title: string, id: number | string): Observable<List> {
+    return this.http.put<List>(`${this.u08ApiUrl}/api/lists/${id}`, title);
+  }
+
+  // delete given list
+  deleteList(id: number | string): Observable<any> {
+    return this.http.delete<any>(`${this.u08ApiUrl}/api/lists/${id}`);
+  }
+
+  // get all recipes belonging to given list
+  getListRecipes(id: number | string): Observable<ListRecipeData> {
+    return this.http.get<ListRecipeData>(`${this.u08ApiUrl}/api/lists/${id}/recipes`);
+  }
+
+  // add a new recipe to given list
+  addRecipeToList(id: number | string, listRecipe: ListRecipe): Observable<ListRecipe> {
+    return this.http.post<ListRecipe>(`${this.u08ApiUrl}/api/lists/${id}/recipes`, listRecipe);
+  }
+
+  // remove given recipe from given list
+  removeRecipeFromList(listId: number | string, recipeId: number | string): Observable<any> {
+    return this.http.delete<any>(`${this.u08ApiUrl}/api/lists/${listId}/recipes/${recipeId}`);
+  }
+
+  // check if given recipe exists in given list
+  checkIfRecipeInList(listId: number | string, recipeId: number | string): Observable<any> {
+    return this.http.get<any>(`${this.u08ApiUrl}/api/lists/${listId}/recipes/${recipeId}`);
+  }
+
+  isRecipeInList(id: number | string) {
+
+  }
+
+  // {
+  //   "success": true,
+  //   "recipes": [
+  //     {
+  //       "id": 5,
+  //       "api_id": 654534,
+  //       "title": "Paneer Makhani",
+  //       "img": "https:\/\/spoonacular.com\/recipeImages\/654534-556x370.jpg",
+  //       "created_at": "2021-08-17T08:32:55.000000Z",
+  //       "updated_at": "2021-08-17T08:32:55.000000Z",
+  //       "pivot": {
+  //         "recipe_list_id": 6,
+  //         "recipe_id": 5
+  //       }
+  //     }
+  //   ]
   // }
 
-  // removeList(): Observable<any> {
-  //   return this.http.get('http://u08-recipe-api.test/api/lists/store');
+  // isRecipeInList(listId: number | string, recipeId: number | string): any {
+  //   let test2;
+  //   const recipe = this.checkIfRecipeInList(listId, recipeId);
+  //   const test = this.checkIfRecipeInList(5, 5).subscribe((data) => {
+  //     test2 = data.exists;
+  //   })
+  //   console.log(test2);
+  //   console.log(test);
   // }
 
-  // updateList(): Observable<any> {
-  //   return this.http.get('http://u08-recipe-api.test/api/lists/store');
-  // }
 
-  // addToList(id: number, user_id: number, title: string, img: string) {
-  //   const idExists = this.lists.some((listItem) => listItem.id === id);
-  //   if (!idExists) {
-  //     this.lists.push({
-  //       id,
-  //       user_id,
-  //       title,
-  //     });
-  //   }
-  // }
 
-  // removeFromList(id: number) {
-  //   const position = this.list.findIndex((recipe) => recipe.id === id);
-  //   if (position >= 0) this.list.splice(position, 1);
-  // }
 
   // checkIfRecipeInList(id: number): boolean {
   //   const idExists = this.list.some((listItem) => listItem.id === id);
