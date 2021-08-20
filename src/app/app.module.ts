651e2a06bf214b99058509ba961a072683f47036
Karin Stenwall <karin.stenwall@gmail.com>
2021-08-20 15:09:04 +0200

    remove: unused dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,18 @@ import { RecipesService } from './services/recipes.service';
 import { EventService } from './services/event.service';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { RecipeComponent } from './components/recipe/recipe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavTabBarComponent } from './components/nav-tab-bar/nav-tab-bar.component';
-import { AppetizersComponent } from './components/appetizers/appetizers.component';
-import { MainsComponent } from './components/mains/mains.component';
-import { DessertsComponent } from './components/desserts/desserts.component';
-import { SidesComponent } from './components/sides/sides.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppetizersComponent } from './pages/appetizers/appetizers.component';
+import { MainsComponent } from './pages/mains/mains.component';
+import { DessertsComponent } from './pages/desserts/desserts.component';
+import { SidesComponent } from './pages/sides/sides.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { DietaryPreferencesComponent } from './components/dietary-preferences/dietary-preferences.component';
-import { RandomRecipesComponent } from './components/random-recipes/random-recipes.component';
-import { ListComponent } from './components/list/list.component';
+import { RandomRecipesComponent } from './pages/random-recipes/random-recipes.component';
+import { ListComponent } from './pages/list/list.component';
 import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 
 import { AuthInterceptor } from './interceptors/auth.interceptor';
@@ -27,16 +27,19 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
-import { SigninComponent } from './components/signin/signin.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { GuestViewComponent } from './components/guest-view/guest-view.component';
 import { AuthViewComponent } from './components/auth-view/auth-view.component';
 import { HeaderXsComponent } from './components/header-xs/header-xs.component';
-import { ListsComponent } from './components/lists/lists.component';
+import { ListsComponent } from './pages/lists/lists.component';
 import { AddListComponent } from './components/add-list/add-list.component';
-import { SaveListDialogComponent } from './components/save-list-dialog/save-list-dialog.component';
 import { SelectListComponent } from './components/select-list/select-list.component';
+import { ListService } from './services/list.service';
+import { NavigationService } from './services/navigation.service';
+import { AuthService } from './services/auth.service';
+import { AuthStateService } from './services/auth-state.service';
+import { TokenService } from './services/token.service';
 
 @NgModule({
   declarations: [
@@ -54,15 +57,13 @@ import { SelectListComponent } from './components/select-list/select-list.compon
     RandomRecipesComponent,
     ListComponent,
     RecipeCardComponent,
-    SigninComponent,
-    SignupComponent,
-    UserProfileComponent,
+    LoginComponent,
+    RegisterComponent,
     GuestViewComponent,
     AuthViewComponent,
     HeaderXsComponent,
     ListsComponent,
     AddListComponent,
-    SaveListDialogComponent,
     SelectListComponent,
   ],
   imports: [
@@ -82,6 +83,11 @@ import { SelectListComponent } from './components/select-list/select-list.compon
     },
     EventService,
     RecipesService,
+    ListService,
+    NavigationService,
+    AuthService,
+    AuthStateService,
+    TokenService,
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
